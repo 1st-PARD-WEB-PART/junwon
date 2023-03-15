9a4d932d86d8f1f6543d11b269ca19fb17e1063a
KJW519 <jwkang519@gmail.com>
2023-03-15 20:13:52 +0900

    1

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          22200019 강준원 마당을 쓸더라도 잘 쓸어라
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
